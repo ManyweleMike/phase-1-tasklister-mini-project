c8ea3a626fb447bf5cb2711a8ed4d6052a58d170
ManyweleMike <mugendimike80@gmail.com>
2022-08-31 07:39:01 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.addEventListener("submit", (e) => {
+    event.preventDefault()
+    const getText = document.getElementById("new-task-description")
+    const li = document.createElement("li")
+    const btn = document.createElement("button")
+    const doneBtn = document.createElement("button")
+    // li.style.listStyleType = "none"
+    btn.innerText = "X"
+    doneBtn.innerText = "✅"
+    doneBtn.style.marginLeft = "25px"
+    doneBtn.style.backgroundColor = "green"
+    doneBtn.style.color = "white"
+    doneBtn.style.border = "none"
+    btn.style.marginLeft = "25px"
+    btn.style.backgroundColor = "red"
+    btn.style.color = "white"
+    btn.style.border = "none"
+    li.append(getText.value, btn, doneBtn)
+    document.getElementById("tasks").append(li)
+  })
 });
